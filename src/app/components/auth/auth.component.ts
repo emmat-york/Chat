aa0730dd6555ef2a3c59fe3274b7f2a907986b9f
emmat-york <andrei.filimonchik@gmail.com>
2022-08-08 21:32:14 +0400

    Chat: Prepare auth component layout

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthPage, PasswordInput } from 'src/app/models/auth.model';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  public authPage: AuthPage = "Sign In";
+  public passwordInputType: PasswordInput = "password";
+  public isSubmitting: boolean;
+  public authFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private readonly formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.initializeAuthForm();
   }
 
+  public onPasswordVisibilityChange(): void {
+    this.passwordInputType = this.passwordInputType === "password" ? "text" : "password";
+  }
+
+  public onChangeAuthPage(event: Event): void {
+    event.preventDefault();
+    this.authPage = this.authPage === "Sign In" ? "Sign Up" : "Sign In";
+  }
+
+  public onFormSubmit(): void {
+    this.isSubmitting = true;
+    console.log("Submit");
+  }
+
+  private initializeAuthForm(): void {
+    this.authFormGroup = this.formBuilder.group({
+      email: [null, Validators.required, Validators.email],
+      password: [null, Validators.required, Validators.maxLength(6)]
+    });
+  }
 }
--- src/app/modules/auth.module.ts
+++ src/app/modules/auth.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { AuthComponent } from '../components/auth/auth.component';
-import { SignUpComponent } from '../components/auth/sign-up/sign-up.component';
-import { SignInComponent } from '../components/auth/sign-in/sign-in.component';
-import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared.module';
 
 @NgModule({
   declarations: [
-    AuthComponent,
-    SignInComponent,
-    SignUpComponent
+    AuthComponent
   ],
-  imports: [CommonModule, RouterModule]
+  imports: [SharedModule]
 })
 export class AuthModule { }
