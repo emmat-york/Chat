bccdac3b94a47ce73a2598b3a74a64d815238365
emmat-york <andrei.filimonchik@gmail.com>
2022-08-25 23:31:05 +0400

    Chat: Prepare 404 page

--- src/app/components/common/not-found/not-found.component.ts
+++ src/app/components/common/not-found/not-found.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
+  constructor(private readonly router: Router) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onBackToMain(): void {
+    this.router.navigate(["/chat"]);
   }
-
 }
