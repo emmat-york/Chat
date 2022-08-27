418c1efc629a76de2f5c5be0eeed440c6e3f9027
emmat-york <andrei.filimonchik@gmail.com>
2022-08-27 14:16:38 +0400

    Blog: Auth errors handling

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,10 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { switchMap, take } from 'rxjs';
+import { catchError, EMPTY, finalize, Observable, switchMap } from 'rxjs';
 import { AuthPage, AuthPayload } from 'src/app/models/auth.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import { UserState } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-auth',
@@ -17,7 +19,7 @@ export class AuthComponent implements OnInit {
   public authFormGroup: FormGroup;
 
   constructor(
-    private readonly authService: AuthService,
+    public readonly authService: AuthService,
     private readonly userService: UserService,
     private readonly formBuilder: FormBuilder,
     private readonly router: Router
@@ -29,6 +31,7 @@ export class AuthComponent implements OnInit {
 
   public onChangeAuthPage(): void {
     this.authPage = this.authPage === "Sign In" ? "Sign Up" : "Sign In";
+    this.authService.authError$.next(null);
     this.authFormGroup.reset();
   }
 
@@ -56,21 +59,27 @@ export class AuthComponent implements OnInit {
   private onSignIn(authPayload: AuthPayload): void {
     this.authService.signIn$(authPayload)
       .pipe(
-        take(1),
+        catchError((error: HttpErrorResponse) => {
+          return this.errorHandle$(error);
+        }),
         switchMap(({ localId }) => {
           return this.userService.getUser$(localId);
+        }),
+        finalize(() => {
+          this.isSubmitting = false;
         })
       )
       .subscribe((userState) => {
-        this.userService.userData$.next(userState);
-        this.router.navigate(["/chat"]);
+        this.onSuccessAuth(userState);
       });
   }
 
   private onSignUp(authPayload: AuthPayload): void {
     this.authService.signUp$(authPayload)
       .pipe(
-        take(1),
+        catchError((error: HttpErrorResponse) => {
+          return this.errorHandle$(error);
+        }),
         switchMap(({ localId, email }) => {
           const initialUserState = {
             isAdmin: false,
@@ -80,12 +89,25 @@ export class AuthComponent implements OnInit {
           };
 
           return this.userService.setUser$(localId, initialUserState);
+        }),
+        finalize(() => {
+          this.isSubmitting = false;
         })
       )
       .subscribe((userState) => {
-        this.userService.userData$.next(userState);
-        this.router.navigate(["/chat"]);
+        this.onSuccessAuth(userState);
       });
+  }
+
+  private onSuccessAuth(userState: UserState): void {
+    this.userService.userData$.next(userState);
+    this.authService.authError$.next(null);
+    this.router.navigate(["/chat"]);
+  }
+
+  private errorHandle$(error: HttpErrorResponse): Observable<never> {
+    this.authService.handleAuthErrors(this.authPage, error);
+    return EMPTY;
   }
 
   private initializeAuthForm(): void {
--- src/app/services/abstract/user.abstract.ts
+++ src/app/services/abstract/user.abstract.ts
@@ -3,5 +3,6 @@ import { UserState } from "src/app/models/user.model";
 
 export abstract class UserInterface {
     public abstract setUser$(localId: string, initialState: UserState): Observable<UserState>;
+    
     public abstract getUser$(localId: string): Observable<UserState>;
 }
